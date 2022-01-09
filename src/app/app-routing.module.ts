c7bfb9271e2897bfb3843c7ba20c78b143ac08fa
Renato <renato.vini@outlook.com>
2022-01-09 17:50:47 -0300

    Rota para componente TECNICO-DELETE

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,50 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { AuthGuard } from "./auth/auth.guard";
+import { HomeComponent } from "./components/home/home.component";
+import { LoginComponent } from "./components/login/login.component";
+import { NavComponent } from "./components/nav/nav.component";
+import { TecnicoCreateComponent } from "./components/tecnico/tecnico-create/tecnico-create.component";
+import { TecnicoDeleteComponent } from "./components/tecnico/tecnico-delete/tecnico-delete.component";
+import { TecnicoListComponent } from "./components/tecnico/tecnico-list/tecnico-list.component";
+import { TecnicoUpdateComponent } from "./components/tecnico/tecnico-update/tecnico-update.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "login",
+    component: LoginComponent,
+  },
+  {
+    path: "",
+    component: NavComponent,
+    canActivate: [AuthGuard],
+    children: [
+      {
+        path: "home",
+        component: HomeComponent,
+      },
+      {
+        path: "tecnicos",
+        component: TecnicoListComponent,
+      },
+      {
+        path: "tecnicos/create",
+        component: TecnicoCreateComponent,
+      },
+      {
+        path: "tecnicos/update/:id",
+        component: TecnicoUpdateComponent,
+      },
+      {
+        path: "tecnicos/delete/:id",
+        component: TecnicoDeleteComponent,
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
